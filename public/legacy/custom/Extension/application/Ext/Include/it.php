dfd88e77a354bce1c92d151f9134ccd798ef553b
AmoxV2 <60665323+AmoxV2@users.noreply.github.com>
2024-04-03 18:24:40 +0200

    Dodanie Modułów IT_claims,it...m It_ServiceNotRegistered

--- public/legacy/custom/Extension/application/Ext/Include/it.php
+++ public/legacy/custom/Extension/application/Ext/Include/it.php
@@ -1,7 +1,26 @@
 <?php 
  //WARNING: The contents of this file are auto-generated
+$beanList['it_Incidents'] = 'it_Incidents';
+$beanFiles['it_Incidents'] = 'modules/it_Incidents/it_Incidents.php';
+$moduleList[] = 'it_Incidents';
+
+
+$beanList['it_Complaiants'] = 'it_Complaiants';
+$beanFiles['it_Complaiants'] = 'modules/it_Complaiants/it_Complaiants.php';
+$moduleList[] = 'it_Complaiants';
+
+
+$beanList['it_Claims'] = 'it_Claims';
+$beanFiles['it_Claims'] = 'modules/it_Claims/it_Claims.php';
+$moduleList[] = 'it_Claims';
+
+
 $beanList['it_ServiceNotRegistered'] = 'it_ServiceNotRegistered';
 $beanFiles['it_ServiceNotRegistered'] = 'modules/it_ServiceNotRegistered/it_ServiceNotRegistered.php';
 $moduleList[] = 'it_ServiceNotRegistered';
+
+
+
+
 
 ?>
