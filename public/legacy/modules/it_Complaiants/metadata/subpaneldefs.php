cc13b0046a4433e4a86f05900051001277d07f06
FireFly0707 <jakubswietliczny@gmail.com>
2024-04-25 19:57:17 +0200

    Spotkanie 25.04.2024

--- public/legacy/modules/it_Complaiants/metadata/subpaneldefs.php
+++ public/legacy/modules/it_Complaiants/metadata/subpaneldefs.php
@@ -12,3 +12,16 @@ $layout_defs[$module_name]['subpanel_setup']['securitygroups'] = array(
     'add_subpanel_data' => 'securitygroup_id',
     'title_key' => 'LBL_SECURITYGROUPS_SUBPANEL_TITLE',
 );
+$layout_defs[$module_name]['subpanel_setup']['service_electronics'] = array(
+    'order' => 100,
+    'module' => 'it_serviceElectronic',
+    'subpanel_name' => 'default',
+    'sort_order' => 'asc',
+    'sort_by' => 'name',
+    'title_key' => 'LBL_SERVICE_SUBPANEL_TITLE',
+    'get_subpanel_data' => 'service_electronics',
+    'top_buttons' => array(
+        array('widget_class' => 'SubPanelTopSelectButton', 'popup_module' => 'it_serviceElectronic', 'mode' => 'OuickCreate'),
+        array('widget_class' => 'SubPanelTopSelectButton', 'popup_module' => 'it_ServiceNotRegistered', 'mode' => 'OuickCreate'),
+    ),
+);
