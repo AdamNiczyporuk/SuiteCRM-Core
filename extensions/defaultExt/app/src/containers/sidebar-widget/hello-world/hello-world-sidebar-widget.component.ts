44b5252639578cbaa366e3e46543e62fefd34899
AndrzejNi <pp.Andrzej.Niczyporuk@gmail.com>
2024-05-22 07:25:07 +0200

    FIX Hello wrold

--- extensions/defaultExt/app/src/containers/sidebar-widget/hello-world/hello-world-sidebar-widget.component.ts
+++ extensions/defaultExt/app/src/containers/sidebar-widget/hello-world/hello-world-sidebar-widget.component.ts
@@ -1,7 +1,5 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
-import {
-    BaseWidgetComponent,
-} from 'core';
+import {BaseWidgetComponent} from 'core';
 
 @Component({
     selector: 'scrm-hello-world-sidebar-widget',
@@ -12,4 +10,13 @@ export class HelloWorldSidebarWidgetComponent extends BaseWidgetComponent implem
     constructor() {
         super();
     }
+    ngOnDestroy(): void {
+        // Tutaj możesz umieścić logikę, która ma być wykonana przy niszczeniu komponentu
+        console.log('HelloWorldSidebarWidgetComponent destroyed');
+    }
+    ngOnInit(): void {
+        // Tutaj możesz umieścić logikę, która ma być wykonana przy tworzeniu  komponentu
+        console.log('HelloWorldSidebarWidgetComponent Init');
+    }
+
 }
