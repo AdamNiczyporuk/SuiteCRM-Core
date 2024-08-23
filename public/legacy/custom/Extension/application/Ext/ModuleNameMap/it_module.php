ed6e465214b3be005326d3737381174829650506
AndrzejNi <pp.Andrzej.Niczyporuk@gmail.com>
2024-08-23 13:27:29 +0200

    Fix table names

---
+++ public/legacy/custom/Extension/application/Ext/ModuleNameMap/it_module.php
@@ -0,0 +1,36 @@
+<?php
+
+$module_name_map = $module_name_map ?? [];
+
+$module_name_map['it_Incidents'] = [
+    'frontend' => 'it_incidents',
+    'core' => 'it_Incidents'
+];
+
+
+$module_name_map['it_Incidents'] = [
+    'frontend' => 'it_incidents',
+    'core' => 'it_Incidents'
+];
+$module_name_map['it_Complaiants'] = [
+    'frontend' => 'it_complaiants',
+    'core' => 'it_Complaiants'
+];
+$module_name_map['it_Claims'] = [
+    'frontend' => 'it_claims',
+    'core' => 'it_Claims'
+];
+$module_name_map['it_ServiceNotRegistered'] = [
+    'frontend' => 'it_servicenotregistered',
+    'core' => 'it_ServiceNotRegistered'
+];
+$module_name_map['it_serviceElectronic'] = [
+    'frontend' => 'it_serviceelectronic',
+    'core' => 'it_serviceElectronic'
+];
+$module_name_map['it_Services'] = [
+    'frontend' => 'it_services',
+    'core' => 'it_Services'
+];
+
+
