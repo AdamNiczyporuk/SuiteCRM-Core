f2b22567ded34ef9af122c5e87e7a9f971125e95
AndrzejNi <pp.Andrzej.Niczyporuk@gmail.com>
2024-09-12 07:18:02 +0200

    ModifiAOS_Product by Custom

--- public/legacy/custom/Extension/modules/AOS_Products/Ext/Vardefs/custom_field.php
+++ public/legacy/custom/Extension/modules/AOS_Products/Ext/Vardefs/custom_field.php
@@ -1,24 +1,25 @@
 <?php
-$dictionary['AOS_Products']['fields']['price']['default']='1';           
+$dictionary['AOS_Products']['fields']['price']['default']=0;           
 $dictionary['AOS_Products']['fields']['type']['default']='Service';
-$dictionary['AOS_Products']['fields']['part_number']['validation']=[
-    'type' => 'callback',
-       'callback' => 'function(formname,nameIndex){if($("#" + nameIndex).val()!=843){add_error_style(formname,nameIndex,"Only 843 is allowed!"); return false;}; return true;}', 
- ];
+//$dictionary['AOS_Products']['fields']['part_number']['validation']=[
+//    'type' => 'callback',
+//       'callback' => 'function(formname,nameIndex){if($("#" + nameIndex).val()!=999){add_error_style(formname,nameIndex,"Only 999 is allowed!"); return false;}; return true;}', 
+// ];
+
 //NOT WORKING  
-$dictionary['AOS_Products']['fields']['cost']['logic']=[
-    'display'=>[
-       'key' => 'displayType',
-       'modes' => ['detail', 'edit', 'create'],
-       'params' => [
-           'fieldDependencies' => [
-               'part_number',
-           ],
-           'targetDisplayType' => 'none',
-           'activeOnFields' =>  ['part_number' => [ '999']]
-       ]
-    ]
-];
+//$dictionary['AOS_Products']['fields']['cost']['logic']=[
+//    'display'=>[
+//       'key' => 'displayType',
+//       'modes' => ['detail', 'edit', 'create'],
+//       'params' => [
+//           'fieldDependencies' => [
+//               'part_number',
+//           ],
+//           'targetDisplayType' => 'none',
+//           'activeOnFields' =>  ['part_number' => [ '999']]
+//       ]
+//    ]
+//];
 
 
  
